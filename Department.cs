a4067791df05c11a6d6a0749e01d5daa784ef47c
StasTikhomirov <Stastih97@gmail.com>
2020-09-19 13:25:32 +0300

    Описывает отдел

---
+++ Department.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithDB
+{
+    /// <summary>
+    /// Описывает сущность - Подразделение
+    /// </summary>
+    class Department : INotifyPropertyChanged
+    {
+      
+        private int departmentId;       
+        private string name;
+
+        /// <summary>
+        /// Код подразделения
+        /// </summary>
+        public int DepartmentId
+        {
+            get { return departmentId; }
+            set
+            {
+                departmentId = value;
+                OnPropertyChanged("DepartmentId");
+            }
+        }
+
+        /// <summary>
+        /// Название подразделения
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+
+        //----------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Обработчик собития изменения свойств класса
+        /// </summary>
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
+
+
