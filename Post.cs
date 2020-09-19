71e29142d8439590ac3b00c7e36f42f449a05097
StasTikhomirov <Stastih97@gmail.com>
2020-09-19 13:40:56 +0300

    Описывают Должность и Статус сотрудника

---
+++ Post.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithDB
+{
+    /// <summary>
+    /// Описывает должность в компании
+    /// </summary>
+    class Post : INotifyPropertyChanged
+    {
+        private int postId;
+        private string name;
+
+        /// <summary>
+        /// Код должности
+        /// </summary>
+        public int PostId
+        {
+            get { return postId; }
+            set
+            {
+                postId = value;
+                OnPropertyChanged(nameof(PostId));
+            }
+        }
+
+        /// <summary>
+        /// Название 
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+
+        //----------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Обработчик собития изменения свойств класса
+        /// </summary>
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ Status.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithDB
+{
+    /// <summary>
+    /// Описывает статус сотрудника в компании
+    /// </summary>
+    class Status : INotifyPropertyChanged
+    {
+        private int statusId;
+        private string name;
+
+        /// <summary>
+        /// Код  статуса
+        /// </summary>
+        public int StatusId
+        {
+            get { return statusId; }
+            set
+            {
+                statusId = value;
+                OnPropertyChanged(nameof(StatusId));
+            }
+        }
+
+        /// <summary>
+        /// Название 
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+
+        //----------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Обработчик собития изменения свойств класса
+        /// </summary>
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
