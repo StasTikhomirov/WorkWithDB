419fba61cbc9862dcf41f669d8b1151d6a03be11
Stastih97 <Stastih97@yandex.ru>
2020-09-20 15:47:26 +0300

    Настройки соединения

---
+++ SQLSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithDB
+{
+    /// <summary>
+    /// Описывает настройки взаимодействия с базой данных
+    /// </summary>
+    public static class SQLSettings
+    {
+        /// <summary>
+        /// Строка подключения к базе данных
+        /// </summary>
+        public static string ConnectionString = @"Data Source=DESKTOP-62FHBOF;Initial Catalog=guide;Integrated Security=True";
+    }
+}
