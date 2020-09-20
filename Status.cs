e08d9685b5792cd278ea74bddfbfb9320204af09
Stastih97 <Stastih97@yandex.ru>
2020-09-20 15:58:44 +0300

    Добавлена связь с базой

--- Status.cs
+++ Status.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace WorkWithDB
 {
@@ -53,6 +55,54 @@ namespace WorkWithDB
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+        //----------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Получение данных о статусах
+        /// </summary>
+        /// <returns>Список статусов сотрудников</returns>
+        public static List<Status> GetStatuses()
+        {
+            List<Status> statuses = new List<Status>();
+
+            // название процедуры
+            string sqlExpression = "dbo.sp_GetStatuses";
+
+            SqlConnection connection = new SqlConnection(SQLSettings.ConnectionString);
+            try
+            {
+                connection.Open();
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show(se.Message, "Ошибка соединения", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                // указываем, что команда представляет хранимую процедуру
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                var reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        statuses.Add(new Status()
+                        {
+                            StatusId = Convert.ToInt32(reader["id"]),
+                            Name = (reader["name"].ToString())
+                        });
+                    }
+                }
+                reader.Close();
+            }
+            connection.Close();
+
+            return statuses;
         }
     }
 }
